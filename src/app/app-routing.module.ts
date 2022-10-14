8402d25b014d6bda99a899d60570cb018a7abfcb
Rafoupeps <rafael.barrot@lmdsio.fr>
2022-10-14 15:55:01 +0200

    reactiv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import {ComposantRouteurComponent}
   from "./composant-routeur/composant-routeur.component";
 import {CalculetteComponent}
   from "./calculette/calculette.component";
+import {SiesteComponent}
+  from "./sieste/sieste.component";
 
 
 const routes: Routes = [
   { path: 'mon_composant', component: ComposantRouteurComponent},
-  {path:'calculette', component: CalculetteComponent}
+  {path:'calculette', component: CalculetteComponent},
+  {path:'sieste', component: SiesteComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComposantRouteurComponent } from './composant-routeur/composant-routeur.component';
@@ -8,13 +7,21 @@ import { ComposantSelecteurComponent } from './composant-selecteur/composant-sel
 import { CalculetteComponent } from './calculette/calculette.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { SiesteComponent } from './sieste/sieste.component';
+import {LaSiesteService} from "./la-sieste.service";
+import { Affichage1Component } from './affichage1/affichage1.component';
+import { Affichage2Component } from './affichage2/affichage2.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ComposantRouteurComponent,
     ComposantSelecteurComponent,
-    CalculetteComponent
+    CalculetteComponent,
+    SiesteComponent,
+    Affichage1Component,
+    Affichage2Component
   ],
   imports: [
     BrowserModule,
@@ -22,7 +29,9 @@ import {HttpClientModule} from "@angular/common/http";
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LaSiesteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
